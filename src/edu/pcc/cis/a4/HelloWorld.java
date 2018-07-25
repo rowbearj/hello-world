5f52ca2c3af984e301e2e93a9bae11957d294976
Robert Layton <robert.layton2@pcc.edu>
2018-07-24 22:27:45 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/a4/HelloWorld.java
+++ src/edu/pcc/cis/a4/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cis.a4;
 
+/**
+ * Created by Robert Layton on 2018.07.24
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
