cf46346526859c0ec745f114cc324da89b042779
Robert Layton <robert.layton2@pcc.edu>
2018-07-24 22:29:27 -0700

    print date added

--- src/edu/pcc/cis/a4/HelloWorld.java
+++ src/edu/pcc/cis/a4/HelloWorld.java
@@ -1,4 +1,5 @@
 package edu.pcc.cis.a4;
+import java.util.Date;
 
 /**
  * Created by Robert Layton on 2018.07.24
@@ -7,5 +8,6 @@ package edu.pcc.cis.a4;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(new Date());
     }
 }
